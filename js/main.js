2c41c2f7e021885f0ed360b00eda9e18221ceee8
Tatiana Limache Quispe <tati34che@gmail.com>
2017-01-16 08:38:28 -0500

    Proyecto al 90%

--- js/main.js
+++ js/main.js
@@ -1,32 +1,24 @@
-/*Todos los campos son obligatorios, excepto los dos últimos.
-Los campos nombre y apellido sólo deben permitir caracteres de la A-Z
-Para los campos nombre y apellido la primera letra debe ser mayúscula
-Validar que el campo email tenga un formato válido. Ej: name@domain.com
-El campo password debe tener al menos 6 caracteres
-El campo password no puede ser igual a "password" ó "123456" ó "098754"
-El valor seleccionado de bicis, debe ser una de las opciones presentadas
-Si algún campo presenta error debe informarse al usuario por medio de un alert.
-*/
+
 //validar que todos los campos esten llenos
-function validateForm()
+function validateForm(form)
 {
+  var name = document.getElementById("name").value;
+  var lastname = document.getElementById("lastname".value);
   var username = document.getElementById("name").value;
   var userlastname = document.getElementById("lastname").value;
   var useremail = document.getElementById("input-email").value;
-  var userpassword = document.getElementById("input-password").value;
-   var salida_enviar = document.getElementById("salida_enviar");
+  var salida_enviar = document.getElementById("salida_enviar");
 
-  if(username.length == 0  || userlastname.length == 0 || useremail.length == 0 || userpassword.length == 0)
-  {
-    salida_enviar.innerHTML="<p style='color:red; font-size:20px;'> Faltan campos por llenar</p>";
-    
+  if(username == "" || userlastname =="" || useremail =="" || validaPass()==false || validaBici()==false){
+      
+    salida_enviar.innerHTML="<p style='color:red; font-size:15px;' >Falta llenar Campos</p>";
     return false;
-  } 
+  }
   else
   {
-    salida_enviar.innerHTML="<p style='color:green; font-size:15px;'>Campos llenados correctamente✔</p>";
-  }
-
+    salida_enviar.innerHTML="<p style='color:green; font-size:15px;' >Formulario enviado ✔</p>";
+    return true;
+  } 
 } 
 
 //validar que solo se escriba letras
@@ -44,21 +36,28 @@ function validaName(form)
 {
   form.l.disabled=(form.n.value=='');
   var nombre = document.getElementById("name");
+  var salida_name = document.getElementById("salida_name");
   var nom=[];
   var minuNombre = nombre.value.slice(1);
   nom=nombre.value;
   nombre.value = nom[0].toUpperCase()+minuNombre;
+  salida_name.innerHTML="<p style='color:green; font-size:15px;' >Nombre valido ✔</p>";
+    
 }
+//valida el nombre segun el formato valido
 function validaLastname(form) 
 {
   //form.e.disabled=(form.l.value=='');
   var apellido = document.getElementById("lastname");
+  var salida_lastname = document.getElementById("salida_lastname");
   var apell=[];
   var minuApellido = apellido.value.slice(1);
   apell=apellido.value;
   apellido.value = apell[0].toUpperCase()+minuApellido;
+  salida_lastname.innerHTML="<p style='color:green; font-size:15px;' >Apellido valido ✔</p>";
+ 
 }
-
+//valida el email segun el formato valido
 function validaEmail(form) 
 {
   
@@ -72,12 +71,13 @@ function validaEmail(form)
   }
   else 
   {
-    salida_mail.innerHTML="<p style='color:green; font-size:20px;'>✔</p>";
+    salida_mail.innerHTML="<p style='color:green; font-size:15px;'>Email valido ✔</p>";
     form.p.disabled=(form.e.value=='');
     return true;
   }
   
 }
+//valida la contraseña, el minimo 6 caracteres
 function validaPass()
 
 {
@@ -88,14 +88,31 @@ function validaPass()
     salida_pass.innerHTML="<p style='color:red; font-size:15px;'>Minimo 6 caracteres</p>";
     return false;
   }
-  else if (password == "password" || password == "123456" || password == "098754") 
+  else if ( password == "123456" || password.toLowerCase() == "password" || password == "098754") 
   {
     salida_pass.innerHTML="<p style='color:red; font-size:15px;' >Contraseña Invalida</p>";
     return false;
   }
   else 
   {
-    salida_pass.innerHTML="<p style='color:green; font-size:20px;'>✔</p>";
+    salida_pass.innerHTML="<p style='color:green; font-size:15px;'>Contraseña valida ✔</p>";
     return true;
+  }
+}
+function validaBici()
+
+{
+  var tipoBici = document.getElementById("select_bici").value;
+  var salida_bici = document.getElementById("salida_bici");
+  if (tipoBici != 0)
+  {
+     salida_bici.innerHTML="<p style='color:green; font-size:15px;'>Bici Seleccionada ✔</p>";
+    return true; 
+  }
+  else 
+  {
+    salida_bici.innerHTML="<p style='color:red; font-size:15px;'>Selecciona una Bici</p>";
+    return false;
+    
   }
 }
