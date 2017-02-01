224486ad27874af408ab455d0730504a31dd0995
Tatiana Limache Quispe <tati34che@gmail.com>
2017-02-01 07:00:25 -0500

    Optimizacion del codigo

--- js/main.js
+++ js/main.js
@@ -1,32 +1,25 @@
-//validar que todos los campos esten llenos
-function validateForm()
-{
+//-------------------------------FUNCION PRINCIPAL LLAMA A LAS DEMAS FUNCIONES-------------------------------------------------//
+//------------------------NO SE PUEDE MANDAR EL FORMULARIO SI TODOS LOS CAMPOS NO SON CORRECTOS--------------------------------//
+function validateForm(){
   var salida_enviar = document.getElementById("salida_enviar");
-  var nombre = document.getElementById("name").value;   
-  var lastname = document.getElementById("lastname").value;    
-  if( nombre.length == 0 || lastname.length == 0 || validaEmail()==false || validaPass()==false || validaBici()==false)
-  { 
-    salida_enviar.innerHTML="<p style='color:red; font-size:15px;' >Falta llenar Campos</p>";
-    return false;
-  }
+  validaName();
+  validaLastname(); 
+  validaEmail(); 
+  validaPass(); 
+  validaBici();
+      
+  if(validaName() == true && validaLastname()==true && validaEmail()==true && validaPass()==true && validaBici()==true)
+      salida_enviar.innerHTML='<p style="color:green;">Formulario Enviado Correctamente ✔</p>';  
   else
-  {
-    salida_enviar.innerHTML="<p style='color:green; font-size:15px;' >Formulario enviado ✔</p>";
-    return true;
-  } 
+     salida_enviar.innerHTML='<p style="color:red;">Llena todos los campos correctamente</p>';
 }
-
-//convierte la primera letra en mayuscula
-function validaName() 
-{
-    var nombre = document.getElementById("name").value;    
-    var nombreArray = nombre.split("");
+//----------------------------------------CONVIERTE LA PRIMERA LETRA EN MAYUSCULA--------------------------------------------------//
+function primMayuscula(id){
+    var nombreArray = id.split("");
     var primeraLetra = nombreArray[0];
     var primeraMayus = primeraLetra.toUpperCase();
     var cortePalabra = false;
-    if(/^[a-zA-Z\s]*$/.test(nombre))
-    {
-        for(var i=1;i<nombreArray.length;i++)
+     for(var i=1;i<nombreArray.length;i++)
         {
           if(cortePalabra)
           {    
@@ -37,102 +30,82 @@ function validaName()
               primeraMayus+=nombreArray[i];
           if(nombreArray[i] == " ")
               cortePalabra = true;
-        } 
-        document.getElementById("name").value = primeraMayus; 
-      salida_name.innerHTML="<p style='color:green; font-size:15px;' >Nombre valido ✔</p>";
-    }
-    else
-    {
-       salida_name.innerHTML="<p style='color:red; font-size:15px;' >Debes escribir letras</p>";
-    }
-  
+        }
+    return primeraMayus;
+} 
+//------------------------------------------VALIDA EL NOMBRE SEGUN FORMATO DADO----------------------------------------------------//
+function validaName(){
+  var name = document.getElementById("name"); 
+  var isValid=false;
+  if(name.value.length > 0 && name.value.match(/^[a-zA-Z\s]*$/)){
+    var palabra=primMayuscula(name.value);
+    document.getElementById("name").value = palabra; 
+    salida_name.innerHTML="<p style='color:green; font-size:15px;' >Nombre valido ✔</p>";
+    isValid=true;
+  }
+  else{
+    salida_name.innerHTML="<p style='color:red; font-size:15px;' >Debes escribir letras en el nombre</p>";
+    isValid=false;
+  }   
+  return isValid;
 }
-//valida el apellido segun el formato valido
-function validaLastname() 
-{
-    var lastname = document.getElementById("lastname").value;    
-    var nombreArray = lastname.split("");
-    var primeraLetra = nombreArray[0];
-    var primeraMayus = primeraLetra.toUpperCase();
-    var cortePalabra = false;
-    if(/^[a-zA-Z\s]*$/.test(lastname))
-    {
-        for(var i=1;i<nombreArray.length;i++)
-        {
-          if(cortePalabra)
-          {    
-            primeraMayus += nombreArray[i].toUpperCase();
-            cortePalabra = false;
-          }
-          else
-              primeraMayus+=nombreArray[i];
-          if(nombreArray[i] == " ")
-              cortePalabra = true;
-        } 
-        document.getElementById("lastname").value = primeraMayus; 
-      salida_lastname.innerHTML="<p style='color:green; font-size:15px;' >Apellido valido ✔</p>";
-    }
-    else
-    {
-     salida_lastname.innerHTML="<p style='color:red; font-size:15px;' >Debes escribir letras</p>";  
-    }
-  
-}
-//valida el email segun el formato valido
-function validaEmail() 
-{
-  
-  var email = document.getElementById("input-email").value;
-  var salida_mail = document.getElementById("salida_mail");
-  
-  if (!email.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) 
-  {
-    salida_mail.innerHTML="<p style='color:red; font-size:15px;' >Formato Invalido, Ej: name@domain.com</p>";
-    return false;
+//------------------------------------------VALIDA EL APELLIDO SEGUN FORMATO DADO-----------------------------------------------------//
+function validaLastname() {
+  var lastname = document.getElementById("lastname");    
+  var isValid=false;
+  if(lastname.value.length > 0 && lastname.value.match(/^[a-zA-Z\s]*$/)){
+    var palabra=primMayuscula(lastname.value);
+    document.getElementById("lastname").value = palabra;  
+    salida_lastname.innerHTML="<p style='color:green; font-size:15px;' >Apellido valido ✔</p>";
+    isValid=true;
   }
-  else 
-  {
-    salida_mail.innerHTML="<p style='color:green; font-size:15px;'>Email valido ✔</p>";
-    return true;
-  }
-  
-}
-//valida la contraseña, el minimo 6 caracteres
-function validaPass()
-
-{
-  var password = document.getElementById("input-password").value;
-  var salida_pass = document.getElementById("salida_pass");
-  if (!password.match(/.{6,}/)) 
-  {
-    salida_pass.innerHTML="<p style='color:red; font-size:15px;'>Minimo 6 caracteres</p>";
-    return false;
-  }
-  else if ( password == "123456" || password.toLowerCase() == "password" || password == "098754") 
-  {
-    salida_pass.innerHTML="<p style='color:red; font-size:15px;' >Contraseña Invalida</p>";
-    return false;
-  }
-  else 
-  { 
-    salida_pass.innerHTML="<p style='color:green; font-size:15px;'>Contraseña valida ✔</p>";
-    return true;
-  }
-}
-function validaBici()
-
-{
-  var tipoBici = document.getElementById("select_bici").value;
-  var salida_bici = document.getElementById("salida_bici");
-  if (tipoBici != 0)
-  {
-     salida_bici.innerHTML="<p style='color:green; font-size:15px;'>Bici Seleccionada ✔</p>";
-    return true; 
-  }
-  else 
-  {
-    salida_bici.innerHTML="<p style='color:red; font-size:15px;'>Selecciona una Bici</p>";
-    return false;
+  else{ 
+   salida_lastname.innerHTML="<p style='color:red; font-size:15px;' >Debes escribir letras en el apellido</p>";  
     
+    isValid=false;
   }
+  return isValid;  
 }
+//-------------------------------------------VALIDA EMAIL SEGUN FORMATO DADO----------------------------------------------------------//
+function validaEmail(){
+  var email = document.getElementById("input-email");
+  var isValid=false;
+ if(email.value.length > 0 && email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){  
+    salida_mail.innerHTML="<p style='color:green; font-size:15px;'>Email valido ✔</p>";
+    isValid=true;
+  }
+  else{
+    salida_mail.innerHTML="<p style='color:red; font-size:15px;' >Formato Invalido, Ej: name@domain.com</p>";
+    isValid=false;
+  }
+  return isValid;
+ }
+//--------------------------------------------VALIDA CONTRASEÑA SEGUN FORMATO DADO---------------------------------------------------------//
+function validaPass(){
+  var password = document.getElementById("input-password");
+  var isValid=false;
+  if (!password.value.match(/.{6,}/) || password.value == "123456" || password.value.toLowerCase() == "password" || password.value == "098754"){
+    salida_pass.innerHTML="<p style='color:red; font-size:15px;'>Debes Ingresar una contraseña valida.</p>";
+    isValid=false;
+  }
+  else{
+   salida_pass.innerHTML="<p style='color:green; font-size:15px;'>Contraseña valida ✔</p>";
+    isValid=true;
+  }
+  return isValid;
+}
+//------------------------------------------VALIDA QUE SELECCIONES UN TIPO DE BICICLETA------------------------------------------------------//
+function validaBici(){
+  var tipo = document.querySelector("select");
+  var isValid=false;    
+  if(tipo.value != 0){
+    salida_bici.innerHTML="<p style='color:green; font-size:15px;'>Bici Seleccionada ✔</p>";
+    isValid=true;
+  } 
+  else{   
+    salida_bici.innerHTML="<p style='color:red; font-size:15px;'>Selecciona una Bici</p>";
+    isValid=false;
+  }
+   return isValid;    
+}
+//-------------------------------------------------------------------FIN------------------------------------------------------------------//
